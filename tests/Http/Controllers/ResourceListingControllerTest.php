1266b3c20faf35d5b46ea6a1aa212c798136cee3
Duncan McClean <duncan@mcclean.co.uk>
2021-07-01 23:11:40 +0100

    wip

---
+++ tests/Http/Controllers/ResourceListingControllerTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace DoubleThreeDigital\Runway\Tests\Http\Controllers;
+
+use DoubleThreeDigital\Runway\Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Statamic\Facades\User;
+
+class ResourceListingControllerTest extends TestCase
+{
+    use RefreshDatabase;
+}
