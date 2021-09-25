fb5f3b6f6b01b038ff0b6327449fed4c062fd5ef
Duncan McClean <duncan@mcclean.co.uk>
2021-09-25 10:10:10 +0100

    Deal with non-blueprint columns (eg. `id`, `created_at`, etc)
    I don't really love the fact DBAL is required for this but can't think of a better way to grab the columns.

--- src/GraphQL/ResourceType.php
+++ src/GraphQL/ResourceType.php
@@ -4,6 +4,7 @@ namespace DoubleThreeDigital\Runway\GraphQL;
 
 use DoubleThreeDigital\Runway\Resource;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Schema;
 use Statamic\Facades\GraphQL;
 use Statamic\GraphQL\Fields\DateField;
 use Rebing\GraphQL\Support\Type;
@@ -21,15 +22,13 @@ class ResourceType extends Type
     public function fields(): array
     {
         return $this->resource->blueprint()->fields()->toGql()
-            ->merge([
-                'id' => ['type' => GraphQL::nonNull(GraphQL::int())],
-                // 'created_at' => new DateField,
-                // 'updated_at' => new DateField,
-            ])
-            ->map(function ($item) {
-                return array_merge($item, [
-                    'resolve' => $this->resolver(),
-                ]);
+            ->merge($this->nonBlueprintFields())
+            ->map(function ($arr) {
+                if (is_array($arr)) {
+                    $arr['resolve'] = $arr['resolve'] ?? $this->resolver();
+                }
+
+                return $arr;
             })
             ->all();
     }
@@ -39,5 +38,41 @@ class ResourceType extends Type
         return function (Model $model, $args, $context, $info) {
             return $model->{$info->fieldName};
         };
+    }
+
+    protected function nonBlueprintFields(): array
+    {
+        $columns = Schema::getConnection()
+            ->getDoctrineSchemaManager()
+            ->listTableColumns($this->resource->databaseTable());
+
+        return collect($columns)
+            ->reject(function ($column) {
+                return in_array(
+                    $column->getName(),
+                    $this->resource->blueprint()->fields()->all()->keys()->toArray()
+                );
+            })
+            ->map(function ($column) {
+                $type = null;
+
+                if ($column->getType() instanceof \Doctrine\DBAL\Types\BigIntType) {
+                    $type = GraphQL::int();
+                }
+
+                if ($column->getType() instanceof \Doctrine\DBAL\Types\DateTimeType) {
+                    // return new DateField;
+                    $type = GraphQL::string();
+                }
+
+                if ($column->getNotnull() === true && ! is_null($type)) {
+                    $type = GraphQL::nonNull($type);
+                }
+
+                return [
+                    'type' => $type,
+                ];
+            })
+            ->toArray();
     }
 }
