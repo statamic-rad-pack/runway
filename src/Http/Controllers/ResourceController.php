0c83b0b818751798787f9c34d220007a7393f08a
Duncan McClean <duncan@mcclean.co.uk>
2021-10-20 17:37:17 +0100

    In fact, just check against `CarbonInterface`

--- src/Http/Controllers/ResourceController.php
+++ src/Http/Controllers/ResourceController.php
@@ -126,7 +126,7 @@ class ResourceController extends CpController
         foreach ($blueprintFieldKeys as $fieldKey) {
             $value = $record->{$fieldKey};
 
-            if ($value instanceof \Carbon\Carbon || $value instanceof \Carbon\CarbonImmutable) {
+            if ($value instanceof \Carbon\CarbonInterface) {
                 $format = $defaultFormat = 'Y-m-d H:i';
 
                 if ($field = $resource->blueprint()->field($fieldKey)) {
