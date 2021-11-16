a5f010dc8c62376aac2a5e7b67ec132d1e348dac
Duncan McClean <duncan@steadfastcollective.com>
2021-11-16 20:41:36 +0000

    Add a test case

--- tests/Http/Controllers/ResourceListingControllerTest.php
+++ tests/Http/Controllers/ResourceListingControllerTest.php
@@ -2,8 +2,10 @@
 
 namespace DoubleThreeDigital\Runway\Tests\Http\Controllers;
 
+use DoubleThreeDigital\Runway\Runway;
 use DoubleThreeDigital\Runway\Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Config;
 use Statamic\Facades\User;
 
 class ResourceListingControllerTest extends TestCase
@@ -75,6 +77,53 @@ class ResourceListingControllerTest extends TestCase
                         'edit_url' => 'http://localhost/cp/runway/post/'.$posts[0]->id,
                         'delete_url' => 'http://localhost/cp/runway/post/'.$posts[0]->id,
                         'id' => $posts[0]->id,
+                    ],
+                ],
+            ]);
+    }
+
+    /** @test */
+    public function can_search_records_with_has_many_relationship()
+    {
+        Config::set('runway.resources.DoubleThreeDigital\Runway\Tests\Author.blueprint.sections.main.fields', [
+            [
+                'handle' => 'name',
+                'field' => [
+                    'type' => 'text',
+                ],
+            ],
+            [
+                'handle' => 'posts',
+                'field' => [
+                    'type' => 'has_many',
+                    'resource' => 'post',
+                    'mode' => 'select',
+                ],
+            ],
+        ]);
+
+        Runway::discoverResources();
+
+        $user = User::make()->makeSuper()->save();
+
+        $author = $this->authorFactory(1, ['name' => 'Colin The Caterpillar']);
+
+        $posts = $this->postFactory(5, ['author_id' => $author->id]);
+
+        $this->actingAs($user)
+            ->get(cp_route('runway.listing-api', [
+                'resourceHandle' => 'author',
+                'search' => 'Colin',
+                'columns' => 'name,posts',
+            ]))
+            ->assertOk()
+            ->assertJson([
+                'data' => [
+                    [
+                        'name' => 'Colin The Caterpillar',
+                        'edit_url' => 'http://localhost/cp/runway/author/'.$author->id,
+                        'delete_url' => 'http://localhost/cp/runway/author/'.$author->id,
+                        'id' => $author->id,
                     ],
                 ],
             ]);
