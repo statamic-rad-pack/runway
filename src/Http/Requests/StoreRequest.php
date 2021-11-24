c724345a1a4eeba409a364596963c9b1fbd1d698
edalzell <erin@silentz.co>
2021-11-24 11:54:20 -0800

    proper permission checks

--- src/Http/Requests/StoreRequest.php
+++ src/Http/Requests/StoreRequest.php
@@ -12,7 +12,7 @@ class StoreRequest extends FormRequest
     {
         $resource = Runway::findResource($this->resourceHandle);
 
-        return User::current()->hasPermission("Create new {$resource->plural()}")
+        return User::current()->hasPermission("Create new {$resource->singular()}")
             || User::current()->isSuper();
     }
 
