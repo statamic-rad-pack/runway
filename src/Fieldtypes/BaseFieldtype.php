5b35a46b198edc8e03555e48aad75931a4785e85
Duncan McClean <duncan@mcclean.co.uk>
2021-12-13 13:35:05 +0000

    Space

--- src/Fieldtypes/BaseFieldtype.php
+++ src/Fieldtypes/BaseFieldtype.php
@@ -98,7 +98,7 @@ class BaseFieldtype extends Relationship
             $fieldtype = $resource->blueprint()->field($column)->fieldtype();
 
             if (! $item instanceof Model) {
-                //In a many to many relation item is an array
+                // In a many to many relation item is an array
                 if (is_array($item)) {
                     $item = $item['id'] ?? null;
                 }
