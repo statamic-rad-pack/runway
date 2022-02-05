84ba3b2db8b28b5796a8c7fce52c99e3eaa913fa
Duncan McClean <duncan@mcclean.co.uk>
2022-02-05 14:07:43 +0000

    Add eager loading to GraphQL index queries
    Co-Authored-By: Daniel Jones <hey@danieldarrenjones.com>

--- src/GraphQL/ResourceIndexQuery.php
+++ src/GraphQL/ResourceIndexQuery.php
@@ -42,6 +42,23 @@ class ResourceIndexQuery extends Query
     {
         $query = $this->resource->model()->newQuery();
 
+        if ($this->resource->hasRouting()) {
+            $query->with('runwayUri');
+        }
+
+        $this->resource->blueprint()->fields()->items()->filter(function ($field) {
+            return $field['field']['type'] === 'belongs_to'
+                || $field['field']['type'] === 'has_many';
+        })->map(function ($field) {
+            if (str_contains($field['handle'], '_id')) {
+                return str_replace('_id', '', $field['handle']);
+            }
+
+            return $field['handle'];
+        })->each(function ($relation) use (&$query) {
+            $query->with($relation);
+        });
+
         $this->filterQuery($query, $args['filter'] ?? []);
         $this->sortQuery($query, $args['sort'] ?? []);
 
@@ -56,7 +73,7 @@ class ResourceIndexQuery extends Query
     protected function filterQuery($query, $filters)
     {
         foreach ($filters as $field => $definitions) {
-            if (! is_array($definitions)) {
+            if (!is_array($definitions)) {
                 $definitions = [['equals' => $definitions]];
             }
 
