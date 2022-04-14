629d962e1e2441b6c09d25f2e0e9566a04f4c210
Duncan McClean <duncan@mcclean.co.uk>
2022-04-14 09:22:24 +0100

    Silly Duncan...

--- src/Http/Resources/ResourceCollection.php
+++ src/Http/Resources/ResourceCollection.php
@@ -92,7 +92,6 @@ class ResourceCollection extends LaravelResourceCollection
 
                 $row['id'] = $record->getKey();
                 $row['edit_url'] = cp_route('runway.edit', ['resourceHandle' => $handle, 'record' => $record->getRouteKey()]);
-                $row['delete_url'] = cp_route('runway.destroy', ['resourceHandle' => $handle, 'record' => $record->getRouteKey()]);
                 $row['permalink'] = $this->runwayResource->hasRouting() ? $record->uri() : null;
                 $row['editable'] = User::current()->hasPermission("Edit {$this->runwayResource->plural()}") || User::current()->isSuper();
                 $row['viewable'] = User::current()->hasPermission("View {$this->runwayResource->plural()}") || User::current()->isSuper();
