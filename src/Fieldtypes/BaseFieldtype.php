ba3a0bfb9c92c929eb0f9019b22c6d73231c1995
Duncan McClean <duncan@steadfastcollective.com>
2022-06-07 11:33:18 +0100

    Store eager loaded relations in Blink cache key

--- src/Fieldtypes/BaseFieldtype.php
+++ src/Fieldtypes/BaseFieldtype.php
@@ -177,7 +177,9 @@ class BaseFieldtype extends Relationship
             })
             ->map(function ($record) use ($resource) {
                 if (! $record instanceof Model) {
-                    $record = Blink::once("Runway::{$this->config('resource')}::{$record}", function () use ($resource, $record) {
+                    $eagerLoadingRelations = collect($this->config('with') ?? [])->join(',');
+
+                    $record = Blink::once("Runway::{$this->config('resource')}::{$record}::{$eagerLoadingRelations}", function () use ($resource, $record) {
                         return $resource->model()
                             ->when($this->config('with'), function ($query) {
                                 $query->with(Arr::wrap($this->config('with')));
