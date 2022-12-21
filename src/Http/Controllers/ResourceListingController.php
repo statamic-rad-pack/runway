a0c10d969e36bcdbf1ba8e0dcb11e62653527de0
Duncan McClean <duncan@duncanmcclean.com>
2022-12-21 15:42:02 +0000

    Simplify code.

--- src/Http/Controllers/ResourceListingController.php
+++ src/Http/Controllers/ResourceListingController.php
@@ -48,8 +48,12 @@ class ResourceListingController extends CpController
                     $query->runwaySearch($searchQuery);
                 },
                 function ($query) use ($searchQuery, $blueprint) {
-                    $blueprint->fields()->items()->reject(fn (array $field) => $field['field']['type'] === 'has_many'
-                        || $field['field']['type'] === 'hidden')->each(function (array $field) use ($query, $searchQuery) {
+                    $blueprint->fields()->items()
+                        ->reject(function (array $field) {
+                            return $field['field']['type'] === 'has_many'
+                                || $field['field']['type'] === 'hidden';
+                        })
+                        ->each(function (array $field) use ($query, $searchQuery) {
                             $query->orWhere($field['handle'], 'LIKE', '%' . $searchQuery . '%');
                         });
                 }
