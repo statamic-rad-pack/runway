9666a4e1c9390c5f26e84eb873f348d8682bfda6
Duncan McClean <19637309+duncanmcclean@users.noreply.github.com>
2023-05-05 11:53:47 +0100

    Add select parameter to Runway tag (#263)
    * Tidy code
    
    * Allow for `select` parameter & cache augmented records in Blink
    
    * Apply Code Style changes
    
    * Add test
    
    ---------
    
    Co-authored-by: duncanmcclean <duncanmcclean@users.noreply.github.com>

--- src/Tags/RunwayTag.php
+++ src/Tags/RunwayTag.php
@@ -8,6 +8,7 @@ use DoubleThreeDigital\Runway\Runway;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 use Statamic\Extensions\Pagination\LengthAwarePaginator;
+use Statamic\Facades\Blink;
 use Statamic\Tags\Tags;
 
 class RunwayTag extends Tags
@@ -27,6 +28,10 @@ class RunwayTag extends Tags
         }
 
         $query = $resource->model()->query();
+
+        if ($select = $this->params->get('select')) {
+            $query->select(explode(',', (string) $select));
+        }
 
         if ($scopes = $this->params->get('scope')) {
             $scopes = explode('|', (string) $scopes);
@@ -116,7 +121,11 @@ class RunwayTag extends Tags
     protected function augmentRecords($query, Resource $resource)
     {
         return collect($query)
-            ->map(fn ($record, $key) => $resource->augment($record))
+            ->map(function ($record, $key) use ($resource) {
+                return Blink::once("Runway::Tag::AugmentRecords::{$resource->handle()}::{$record->{$resource->primaryKey()}}", function () use ($resource, $record) {
+                    return $resource->augment($record);
+                });
+            })
             ->toArray();
     }
 
