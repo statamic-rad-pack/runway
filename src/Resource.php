08515eeee75f063aa5760978ac571b0957a1c7fe
Duncan McClean <19637309+duncanmcclean@users.noreply.github.com>
2023-05-09 21:31:03 +0100

    Belongs To Fieldtype: Allow overriding Eloquent relationship names (#270)
    * Allow overriding Eloquent relationship names
    
    For times when the magic isn't smart enough.
    
    * Add config option

--- src/Resource.php
+++ src/Resource.php
@@ -240,6 +240,14 @@ class Resource
                         ->mapWithKeys(function ($field) {
                             $relationName = $field['handle'];
 
+                            // If the field has a `relationship_name` config key, use that instead.
+                            // Sometimes a column name will be different to the relationship name
+                            // (the method on the model) so our magic won't be able to figure out what's what.
+                            // Eg. standard_parent_id -> parent
+                            if (isset($field['field']['relationship_name'])) {
+                                $relationName = $field['field']['relationship_name'];
+                            }
+
                             // If field handle is `author_id`, strip off the `_id`
                             if (str_contains($relationName, '_id')) {
                                 $relationName = Str::replaceLast('_id', '', $relationName);
