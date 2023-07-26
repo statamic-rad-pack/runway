54b0d34950b34869d3495082eb42d6ac2b6467c1
duncanmcclean <duncanmcclean@users.noreply.github.com>
2023-07-26 18:24:23 +0000

    Apply Code Style changes

--- tests/Console/Commands/GenerateMigrationTest.php
+++ tests/Console/Commands/GenerateMigrationTest.php
@@ -65,7 +65,7 @@ class GenerateMigrationTest extends TestCase
                                             'type' => 'integer',
                                             'validate' => 'required',
                                         ],
-                                    ]
+                                    ],
                                 ],
                             ],
                         ],
