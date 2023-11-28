d49fbbc12dc6fe64acdc99f0c15d2377ecdfd077
Duncan McClean <19637309+duncanmcclean@users.noreply.github.com>
2023-11-28 22:15:27 +0000

    [5.x] Fix search supplements (#377)
    * Fix search supplements
    
    * Try without DatabaseMigrations trait
    
    * Fix styling
    
    ---------
    
    Co-authored-by: duncanmcclean <duncanmcclean@users.noreply.github.com>

--- src/Search/Searchable.php
+++ src/Search/Searchable.php
@@ -26,6 +26,7 @@ class Searchable implements Augmentable, ContainsQueryableValues, Contract
     {
         $this->model = $model;
         $this->resource = Runway::findResourceByModel($model);
+        $this->supplements = collect();
     }
 
     public function resource()
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,7 +7,6 @@ use DoubleThreeDigital\Runway\ServiceProvider;
 use DoubleThreeDigital\Runway\Traits\HasRunwayResource;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Encryption\Encrypter;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
@@ -19,7 +18,7 @@ use Statamic\Statamic;
 
 abstract class TestCase extends OrchestraTestCase
 {
-    use DatabaseMigrations, RefreshDatabase, WithFaker;
+    use RefreshDatabase, WithFaker;
 
     protected $shouldFakeVersion = true;
 
