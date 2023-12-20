4e4f33daa9e2a9f075d99db38ad2e6addd2749f2
edalzell <erin@silentz.co>
2023-12-20 09:46:22 -0800

    pass the model to all of them

--- src/Policies/ResourcePolicy.php
+++ src/Policies/ResourcePolicy.php
@@ -7,25 +7,25 @@ use Statamic\Facades\User;
 
 class ResourcePolicy
 {
-    public function view($user, Resource $resource)
+    public function view($user, Resource $resource, $model = null)
     {
         return User::fromUser($user)
             ->hasPermission("view {$resource->handle()}");
     }
 
-    public function create($user, Resource $resource)
+    public function create($user, Resource $resource, $model = null)
     {
         return User::fromUser($user)
             ->hasPermission("create {$resource->handle()}");
     }
 
-    public function edit($user, Resource $resource)
+    public function edit($user, Resource $resource, $model = null)
     {
         return User::fromUser($user)
             ->hasPermission("edit {$resource->handle()}");
     }
 
-    public function delete($user, Resource $resource)
+    public function delete($user, Resource $resource, $model = null)
     {
         return User::fromUser($user)
             ->hasPermission("delete {$resource->handle()}");
