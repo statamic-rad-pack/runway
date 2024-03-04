afdd7fefdcbb64259fc4a0db409e63713b4c3d04
Erin Dalzell <erin@silentz.co>
2024-03-04 12:49:31 -0800

    it’s `model` in the route binding (#453)

--- src/Http/Requests/CP/UpdateRequest.php
+++ src/Http/Requests/CP/UpdateRequest.php
@@ -15,7 +15,7 @@ class UpdateRequest extends FormRequest
             return false;
         }
 
-        $model = $resource->model()->find($this->record);
+        $model = $resource->model()->find($this->model);
 
         return User::current()->can('edit', [$resource, $model]);
     }
