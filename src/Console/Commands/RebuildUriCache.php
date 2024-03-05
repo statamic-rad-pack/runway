57c555ec13ef32b335c1c0ca505cb1fbf9592c95
Matthew Fenlon <matthewfenlon@gmail.com>
2024-03-05 13:42:18 +0000

    Use scope to limit uri generation.

--- src/Console/Commands/RebuildUriCache.php
+++ src/Console/Commands/RebuildUriCache.php
@@ -66,7 +66,8 @@ class RebuildUriCache extends Command
                     return;
                 }
 
-                $this->withProgressBar($resource->model()->all(), function ($model) use ($resource) {
+                $query = $resource->model();
+                $this->withProgressBar($query->when($query->hasNamedScope('runwayRoutes'), fn ($query) => $query->runwayRoutes())->get(), function ($model) use ($resource) {
                     $uri = (new Parser())
                         ->parse($resource->route(), $model->toAugmentedArray())
                         ->__toString();
