31553f869a24ef50a9bcc987ce5decc5794de58b
Duncan McClean <duncan@duncanmcclean.com>
2024-05-03 15:28:13 +0100

    Remove un-used `RelationshipFieldtype` component

--- resources/js/cp.js
+++ resources/js/cp.js
@@ -1,10 +1,7 @@
 import PublishForm from './components/Publish/PublishForm.vue'
 import RunwayListing from './components/Listing/RunwayListing.vue'
+import HasManyFieldtypeIndex from '../../vendor/statamic/cms/resources/js/components/fieldtypes/relationship/RelationshipIndexFieldtype.vue'
 
 Statamic.$components.register('runway-publish-form', PublishForm)
 Statamic.$components.register('runway-listing', RunwayListing)
-
-// Statamic seems to form the fieldtype name differently, so we're just pointing it where it should be going so stuff works.
-import HasManyFieldtypeIndex from '../../vendor/statamic/cms/resources/js/components/fieldtypes/relationship/RelationshipIndexFieldtype.vue'
-
 Statamic.$components.register('has_many-fieldtype-index', HasManyFieldtypeIndex)
