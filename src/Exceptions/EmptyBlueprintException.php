3e4f0b948e353ea33b8d98cd02303bec4d2f31c4
Duncan McClean <duncan@duncanmcclean.com>
2024-05-17 18:04:12 +0100

    Improve empty blueprint exception

--- src/Exceptions/EmptyBlueprintException.php
+++ src/Exceptions/EmptyBlueprintException.php
@@ -2,10 +2,24 @@
 
 namespace StatamicRadPack\Runway\Exceptions;
 
-class EmptyBlueprintException extends \Exception
+use Spatie\Ignition\Contracts\ProvidesSolution;
+use Spatie\Ignition\Contracts\Solution;
+use Spatie\Ignition\Contracts\BaseSolution;
+
+class EmptyBlueprintException extends \Exception implements ProvidesSolution
 {
     public function __construct(protected string $resourceHandle)
     {
-        parent::__construct("The blueprint for the {$this->resourceHandle} resource is empty. Please add fields to the blueprint.");
+        parent::__construct("There are no fields defined in the {$this->resourceHandle} blueprint.");
+    }
+
+    public function getSolution(): Solution
+    {
+        return BaseSolution::create("Add fields to the {$this->resourceHandle} blueprint")
+            ->setSolutionDescription('Before you can view this resource in the Control Panel, you need to define fields in its blueprint.')
+            ->setDocumentationLinks([
+                'Edit blueprint' => cp_route('blueprints.edit', ['namespace' => 'runway', 'handle' => $this->resourceHandle]),
+                'Review the docs' => 'https://runway.duncanmcclean.com/blueprints',
+            ]);
     }
 }
