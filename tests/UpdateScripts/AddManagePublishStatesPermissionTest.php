c08fa82564b6631d2f00dfe674999af8e6426e8f
Duncan McClean <duncan@duncanmcclean.com>
2024-07-12 17:00:31 +0100

    add tests for the update script

---
+++ tests/UpdateScripts/AddManagePublishStatesPermissionTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace StatamicRadPack\Runway\Tests\UpdateScripts;
+
+use Illuminate\Support\Facades\File;
+use PHPUnit\Framework\Attributes\Test;
+use Statamic\Facades\Role;
+use Statamic\Facades\YAML;
+use StatamicRadPack\Runway\Tests\TestCase;
+use StatamicRadPack\Runway\UpdateScripts\AddManagePublishStatesPermission;
+use StatamicRadPack\Runway\UpdateScripts\ChangePermissionNames;
+
+class AddManagePublishStatesPermissionTest extends TestCase
+{
+    use RunsUpdateScripts;
+
+    #[Test]
+    public function publish_permission_is_added_to_role_with_create_permission()
+    {
+        Role::make('test')
+            ->addPermission('view post')
+            ->addPermission('create post')
+            ->save();
+
+        $this->runUpdateScript(AddManagePublishStatesPermission::class);
+
+        $this->assertEquals([
+            'view post',
+            'create post',
+            'publish post',
+        ], Role::find('test')->permissions()->all());
+    }
+
+    #[Test]
+    public function publish_permission_is_not_added_to_role_without_create_permission()
+    {
+        Role::make('test')
+            ->addPermission('view post')
+            ->save();
+
+        $this->runUpdateScript(AddManagePublishStatesPermission::class);
+
+        $this->assertEquals([
+            'view post',
+        ], Role::find('test')->permissions()->all());
+    }
+}
