6f50d54d534b125b33181908726371288e3fb5c8
indykoning <15870933+indykoning@users.noreply.github.com>
2024-10-11 16:59:30 +0200

    [7.x] Minor optimisations on isJson function (#619)
    Co-authored-by: Duncan McClean <duncan@duncanmcclean.com>

--- src/Support/Json.php
+++ src/Support/Json.php
@@ -6,10 +6,15 @@ class Json
 {
     public static function isJson($value): bool
     {
-        if (is_array($value) || is_object($value)) {
+        if (
+            is_array($value)
+            || is_object($value)
+            || (is_string($value) && ! str_starts_with($value, '[') && ! str_starts_with($value, '{'))
+        ) {
             return false;
         }
 
+        // TODO: Replace this with json_validate when dropping support for PHP 8.2.
         return is_array(json_decode($value, true));
     }
 }
