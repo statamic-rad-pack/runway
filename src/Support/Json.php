fd9fd7cf86c973baaf30dccbd8172a74c7fa2cfb
indykoning <15870933+indykoning@users.noreply.github.com>
2024-10-24 16:10:56 +0200

    Add check whether value is null

--- src/Support/Json.php
+++ src/Support/Json.php
@@ -7,7 +7,8 @@ class Json
     public static function isJson($value): bool
     {
         if (
-            is_array($value)
+            $value === null
+            || is_array($value)
             || is_object($value)
             || (is_string($value) && ! str_starts_with($value, '[') && ! str_starts_with($value, '{'))
         ) {
