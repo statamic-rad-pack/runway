a1a97da919a0febee5bb9a7f6218a0cd73475c26
duncanmcclean <duncanmcclean@users.noreply.github.com>
2025-01-23 11:53:45 +0000

    Fix styling

--- src/Resource.php
+++ src/Resource.php
@@ -4,7 +4,6 @@ namespace StatamicRadPack\Runway;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Str;
 use Statamic\Facades\Blink;
