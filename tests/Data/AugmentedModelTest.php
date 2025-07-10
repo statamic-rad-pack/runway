13e2bfdbe516780ebb3f753b5727062de45a7e02
Duncan McClean <duncan@duncanmcclean.com>
2025-07-10 12:14:24 +0100

    Improve nested field augmentation

--- tests/Data/AugmentedModelTest.php
+++ tests/Data/AugmentedModelTest.php
@@ -45,7 +45,7 @@ class AugmentedModelTest extends TestCase
     }
 
     #[Test]
-    public function it_gets_nested_values()
+    public function it_gets_nested_values_via_nested_field_prefix()
     {
         $post = Post::factory()->create([
             'values' => [
@@ -58,8 +58,28 @@ class AugmentedModelTest extends TestCase
 
         $this->assertIsArray($augmented->get('values')->value());
 
+        // Eg. values:alt_title, values:alt_body
         $this->assertEquals('Alternative Title...', $augmented->get('values')->value()['alt_title']->value());
         $this->assertEquals('<p>This is a <strong>great</strong> post! You should <em>read</em> it.</p>', trim($augmented->get('values')->value()['alt_body']->value()));
+    }
+
+    #[Test]
+    public function it_gets_nested_values_via_nested_field_handles()
+    {
+        $post = Post::factory()->create([
+            'values' => [
+                'alt_title' => 'Alternative Title...',
+                'alt_body' => 'This is a **great** post! You should *read* it.',
+            ],
+        ]);
+
+        $augmented = new AugmentedModel($post);
+
+        $this->assertIsArray($augmented->get('values')->value());
+
+        // Eg. values_alt_title, values_alt_body
+        $this->assertEquals('Alternative Title...', $augmented->get('values_alt_title')->value());
+        $this->assertEquals('<p>This is a <strong>great</strong> post! You should <em>read</em> it.</p>', trim($augmented->get('values_alt_body')->value()));
     }
 
     #[Test]
