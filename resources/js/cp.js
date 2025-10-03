25801ee69c47a37934ff43f33182b1cb0ffcd86c
Duncan McClean <duncan@duncanmcclean.com>
2025-10-03 23:25:11 +0100

    wip

--- resources/js/cp.js
+++ resources/js/cp.js
@@ -1,17 +1,17 @@
-import RelatedItem from './components/inputs/relationship/Item.vue';
-import PublishForm from './components/resources/PublishForm.vue';
-import ResourceWidget from './components/resources/Widget.vue';
-
 import Index from './pages/Index.vue';
 import Create from './pages/Create.vue';
 import Edit from './pages/Edit.vue';
 
-Statamic.booting(() => {
-    Statamic.$components.register('runway-related-item', RelatedItem);
-    Statamic.$components.register('runway-publish-form', PublishForm);
-    Statamic.$components.register('runway-resource-widget', ResourceWidget);
+import RelatedItem from './components/inputs/relationship/Item.vue';
+import PublishForm from './components/resources/PublishForm.vue';
+import ResourceWidget from './components/resources/Widget.vue';
 
+Statamic.booting(() => {
     Statamic.$components.register('Pages/Runway/Index', Index);
     Statamic.$components.register('Pages/Runway/Create', Create);
     Statamic.$components.register('Pages/Runway/Edit', Edit);
+
+    Statamic.$components.register('runway-related-item', RelatedItem);
+    Statamic.$components.register('runway-publish-form', PublishForm);
+    Statamic.$components.register('runway-resource-widget', ResourceWidget);
 });
