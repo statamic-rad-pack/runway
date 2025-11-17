b1f5118380e9deffd57f409fb8c652e7316ed4cc
Duncan McClean <duncan@duncanmcclean.com>
2025-11-17 17:26:24 +0000

    formatting

--- tests/__fixtures__/database/migrations/2020_12_12_220453_create_posts_table.php
+++ tests/__fixtures__/database/migrations/2020_12_12_220453_create_posts_table.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use StatamicRadPack\Runway\Tests\Fixtures\Enums\MembershipStatus;
 
 class CreatePostsTable extends Migration
 {
