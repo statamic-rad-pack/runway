02afd9024150bb0fb44fd95012af29275617b0b6
Duncan McClean <duncan@duncanmcclean.com>
2026-01-12 09:58:16 +0000

    Render `ResourceNotFound` exception as a 404

--- src/Exceptions/ResourceNotFound.php
+++ src/Exceptions/ResourceNotFound.php
@@ -2,10 +2,19 @@
 
 namespace StatamicRadPack\Runway\Exceptions;
 
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Statamic\Exceptions\NotFoundHttpException;
+
 class ResourceNotFound extends \Exception
 {
     public function __construct(protected string $resourceHandle)
     {
         parent::__construct("Runway could not find [{$resourceHandle}]. Please ensure its configured properly and you're using the correct handle.");
+    }
+
+    public function render(Request $request): Response
+    {
+        throw new NotFoundHttpException($this->message);
     }
 }
