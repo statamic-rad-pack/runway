8479e033fccecc3667c1609ff0b8c37c345ae624
Duncan McClean <duncan@duncanmcclean.com>
2026-03-13 14:04:29 +0100

    [9.x] Fix error when duplicating model with "computed" title (#788)

--- src/Actions/DuplicateModel.php
+++ src/Actions/DuplicateModel.php
@@ -68,7 +68,7 @@ class DuplicateModel extends Action
     {
         $model = $original->replicate($resource->blueprint()->fields()->all()->reject->shouldBeDuplicated()->keys()->all());
 
-        if ($resource->titleField()) {
+        if ($resource->titleField() && in_array($resource->titleField(), $resource->databaseColumns())) {
             $model->setAttribute($resource->titleField(), $original->getAttribute($resource->titleField()).' (Duplicate)');
         }
 
