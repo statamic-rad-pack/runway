a4294cff9af5d2b2b1de5a93b88de4de96892052
Duncan McClean <duncan@duncanmcclean.com>
2026-03-26 11:46:14 +0000

    use existing sort column & direction from resource config array

--- tests/__fixtures__/app/Models/Author.php
+++ tests/__fixtures__/app/Models/Author.php
@@ -33,16 +33,6 @@ class Author extends Model
         }
     }
 
-    public function sortColumn(): string
-    {
-        return 'name';
-    }
-
-    public function sortDirection(): string
-    {
-        return 'desc';
-    }
-
     protected static function newFactory()
     {
         return AuthorFactory::new();
