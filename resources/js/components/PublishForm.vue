da0a6d7ea5d35f2f807a85c6c27aee0b2c3451b6
Mason Curry <mason.curry1@gmail.com>
2026-05-28 04:41:12 -0500

    [9.x] Fix belongs_to not being populated during inline has_many creation (#823)

--- resources/js/components/PublishForm.vue
+++ resources/js/components/PublishForm.vue
@@ -474,7 +474,7 @@ export default {
         },
 
         parentContainer() {
-            return this.publishContext?.parentContainer;
+            return this.publishContext;
         },
     },
 
